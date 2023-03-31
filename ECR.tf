1cc059a9b4c711f56015c89c543bfb3c05046951
nara85 <112981729+nara85@users.noreply.github.com>
2023-03-31 19:40:12 +0400

    Update ECR.tf

--- ECR.tf
+++ ECR.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "team2-ECR" {
-  name                 = "my-ecr-repo"
+  name                 = "my-exam1-repo"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
